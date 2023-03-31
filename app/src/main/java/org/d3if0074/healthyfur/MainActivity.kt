71963cf0d519a6632c2c26f03b531e3dcfd8a792
silvyanarisma <silvyana.risma@gmail.com>
2023-03-31 10:24:28 +0700

    Menambahkan view binding

--- app/src/main/java/org/d3if0074/healthyfur/MainActivity.kt
+++ app/src/main/java/org/d3if0074/healthyfur/MainActivity.kt
@@ -2,10 +2,19 @@ package org.d3if0074.healthyfur
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.d3if0074.healthyfur.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+
 }
