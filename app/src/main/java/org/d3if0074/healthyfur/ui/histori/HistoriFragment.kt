1d5a079a7812daef55db7f6f5a8edbf8f0d438f4
silvyanarisma <silvyana.risma@gmail.com>
2023-05-22 18:00:30 +0700

    Menyimpan preference dengan data store

--- app/src/main/java/org/d3if0074/healthyfur/ui/histori/HistoriFragment.kt
+++ app/src/main/java/org/d3if0074/healthyfur/ui/histori/HistoriFragment.kt
@@ -2,17 +2,28 @@ package org.d3if0074.healthyfur.ui.histori
 
 import android.os.Bundle
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.launch
 import org.d3if0074.healthyfur.R
 import org.d3if0074.healthyfur.databinding.FragmentHistoriBinding
 import org.d3if0074.healthyfur.db.HealthyFurDb
-import java.util.zip.Inflater
+import org.d3if0074.healthyfur.db.SettingDataStore
+import org.d3if0074.healthyfur.db.dataStore
 
 class HistoriFragment: Fragment() {
+    private val layoutDataStore: SettingDataStore by lazy {
+        SettingDataStore(requireContext().dataStore)
+    }
+
     private val viewModel: HistoriViewModel by lazy {
         val db = HealthyFurDb.getInstance(requireContext())
         val factory = HistoriViewModelFactory(db.dao)
@@ -23,6 +34,8 @@ class HistoriFragment: Fragment() {
 
     private lateinit var myAdapter: HistoriAdapter
 
+    private var isLinearLayout = true
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHistoriBinding.inflate(layoutInflater, container, false)
@@ -31,6 +44,11 @@ class HistoriFragment: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        layoutDataStore.preferenceFlow.asLiveData().observe(viewLifecycleOwner) {
+            isLinearLayout = it
+            setLayout()
+            activity?.invalidateOptionsMenu()
+        }
         myAdapter = HistoriAdapter(this)
         with(binding.recyclerView) {
             addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
@@ -47,11 +65,20 @@ class HistoriFragment: Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.histori_menu, menu)
+
+        val menuItem = menu.findItem(R.id.action_switch_layout)
+        setIcon(menuItem)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.menu_hapus) {
             deleteAllData()
+            return true
+        }
+        if (item.itemId == R.id.action_switch_layout) {
+            lifecycleScope.launch {
+                layoutDataStore.saveLayout(!isLinearLayout, requireContext())
+            }
             return true
         }
         return super.onOptionsItemSelected(item)
@@ -68,4 +95,19 @@ class HistoriFragment: Fragment() {
             }
             .show()
     }
+
+    private fun setLayout() {
+        binding.recyclerView.layoutManager = if (isLinearLayout)
+            LinearLayoutManager(context)
+        else
+            GridLayoutManager(context, 2)
+    }
+    private fun setIcon(menuItem: MenuItem) {
+        val iconId = if (isLinearLayout)
+            R.drawable.baseline_grid_view_24
+        else
+            R.drawable.baseline_view_list_24
+        menuItem.icon = ContextCompat.getDrawable(requireContext(), iconId)
+    }
+
 }
